d357bbbe6ba3510e52aaf51d8fd8f1fd2aa48ef1
RomanManianov <r.manianov@ya.ru>
2023-03-23 09:52:50 +0300

    ORG-38: связь суперадмина со стримером

--- src/main/resources/db/migration/2023/02/ORG-9/1_create_table_admins_streamers_links.sql
+++ src/main/resources/db/migration/2023/02/ORG-9/1_create_table_admins_streamers_links.sql
@@ -2,8 +2,8 @@ CREATE TABLE admins_streamers
 (
     id BIGSERIAL NOT NULL,
     admin_code VARCHAR(30) NOT NULL,
+    super_admin_code VARCHAR(30) NOT NULL,
     streamer_code VARCHAR(30) NOT NULL,
-    isSuperAdmin BOOLEAN NOT NULL,
 
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     active BOOLEAN NOT NULL DEFAULT true
